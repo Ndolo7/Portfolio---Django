20b54857992d8d6c6c8fc2e395ddba2ac503a2ff
Ndolo <imanindolo77@gmail.com>
2024-11-29 20:50:46 +0300

    added host

--- mypage/settings.py
+++ mypage/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = 'django-insecure-v2d=i3)ca2_(%4_q^8ta*8-o@d@jy=yql0=5lvv#j454x%+)z3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'imaniportfolio-lpbmow4i.b4a.run', 'ndolo7.github.io']
+ALLOWED_HOSTS = ['127.0.0.1',
+                 '.vercel.app', 
+                 'imaniportfolio-lpbmow4i.b4a.run', 
+                 'ndolo7.github.io',
+                 'node293a.containers.back4app.com'
+                ]
 
 
 # Application definition
